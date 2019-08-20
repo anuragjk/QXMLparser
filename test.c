6fa93537463f8ea476df71a3059af183fb2eb993
anuragjk <anurag.zxcv@gmail.com>
2019-08-20 20:17:58 +0530

    Fixed minor bug with psStartBkmrk and Fixed filename in file header for QXMLParser.c

---
+++ test.c
@@ -0,0 +1,21 @@
+#include "QXMLParser.h"
+#include <stdio.h>
+
+void main()
+{
+	tXmlParser var;
+	char value[32] = {0};	
+        int err;
+
+	QXMLinit(&var, "sample.xml");
+	if( QXML_OK == (err = QXMLparse(&var, "LINE1", value) ))
+	{
+		printf("Found: >%s<\n", value);
+	}
+	else
+	{
+		printf("not Found!\n");
+		printf("Err: %d\n",err);
+	}
+	QXMLdestroy(&var);
+}
